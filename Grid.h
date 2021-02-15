e07a435beb4d3b499f8210ae7291339e90de8963
Nina Kahr <nina.kahr20@imperial.ac.uk>
2021-02-15 19:47:38 +0100

    Make Grid property cells public

--- Grid.h
+++ Grid.h
@@ -14,13 +14,14 @@ private:
     std::string data = "";
     std::string new_data = "";
 
-    std::vector<bool> cells;
     std::vector<bool> new_cells;
 
     bool do_iteration_parallel();
     bool do_iteration_serial();
 
 public:
+    std::vector<bool> cells;
+
     Grid(const int &num_rows, const int &num_cols, bool is_parallel = true);
     Grid(const int &num_rows, const int &num_cols, std::vector<bool> &cell_states, bool is_parallel = true);
     ~Grid();
